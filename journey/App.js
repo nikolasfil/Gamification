b163cc3e495e3fcd9e8fec9555be2e126c810dd4
nikolasfil <nikolas13in@gmail.com>
2023-05-06 15:54:50 +0300

    working on changing the pages ;

--- journey/App.js
+++ journey/App.js
@@ -1,30 +1,86 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, {useState} from 'react';
+import { View, StyleSheet, Text, Button } from 'react-native';
 
 
 const App = () => {
+
+  const [pages,setPage] = useState([
+    {name: 'Home', page: HomePage},
+    {name: 'About', page: AboutPage},
+    {name: 'Profile', page: ProfilePage},
+  ]);
+
+
+  const clickMenu = () => {
+    console.log('clickMenu');
+  }
+
   return (
-    <View style={styles.container}>
-      {/* <View style={styles.navigation}> */}
-        <Text > This is text</Text>
-      {/* </View> */}
+
+    <View style={styles.navigation}>
+      <Text>Home</Text>
+
+      <PageSelector
+        value='test' 
+        >
+      </PageSelector>
     </View>
+
+    // <PageSelector>
+    //   <View>
+    //     <Text>This is an example</Text>
+    //     </View>
+    //   </PageSelector>
   );
 };
 
+
+const PageSelector = ({value}) => (
+
+  <View >
+
+    <View >
+      <Text>{value}</Text>
+    </View>
+  </View>
+
+);
+
+const ProfilePage = () => (
+  <View >
+      <Text>Profile</Text>
+    </View>
+);
+
+
+const AboutPage = () => (
+  <View >
+      <Text>Profile</Text>
+    </View>
+);
+
+const HomePage = () => (
+  <View >
+      <Text>Profile</Text>
+    </View>
+);
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // flex: 1,
+    marginTop: 40,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    // alignItems: 'center',
   },
 
   navigation: {
-    flex: 1,
-    // flexDirection: 'row',
-    // justifyContent: 'space-around',
+    // flex: 1,
+    marginTop: 40,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
     backgroundColor: '#fff',
-    alignItems: 'center',
+    // alignItems: 'center',
     // justifyContent: 'center',
   },
 });
