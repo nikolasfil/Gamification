aef88055fccb2c0ee5c78b3aec4919ea7f5cb0f6
nikolasfil <nikolas13in@gmail.com>
2023-05-06 12:22:04 +0300

    tutorial for react native

--- journey/App.js
+++ journey/App.js
@@ -1,12 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Button} from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
+  const [name, setName] = useState('shau');
+
+  const [person, setPerson]  = useState({name: 'mario', age: 40});
+
+
+
+  const clickHandler = () => {
+    setName('li');
+    setPerson({name: 'luigi', age: 45});
+
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>THis is Journey</Text>
-      <StatusBar style="auto" />
+      <Text>THis is {name}</Text>
+      <Text>His name is {person.name} and his age is {person.age}</Text>
+
+
+      <StatusBar style="auto"/>
+      <View style={styles.buttonContainer}>
+        {/* <Button title="update state" onPress={() => setName('shaun')}/> */}
+        <Button title="update state" onPress={clickHandler}/>
+
+      
+      
+      </View>
     </View>
   );
 }
@@ -18,4 +40,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer: {
+    marginTop: 20
+  },
+
 });
